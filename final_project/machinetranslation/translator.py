7d4cd410645b964b3649eefef3a893d137cf06ad
“Yudha <yudhae@gmail.com>
2023-06-14 00:32:26 -0400

    Final Update

--- final_project/machinetranslation/translator.py
+++ final_project/machinetranslation/translator.py
@@ -47,3 +47,31 @@ def frenchToEnglish(frenchText):
     englishText = translation.get('translations')[0]\
         .get('translation')
     return englishText
+
+import json
+# import os
+# from deep_translator import MyMemoryTranslator
+
+
+# # english to french
+# def english_to_french(english_text):
+#     """
+#     function to translate Englist to french
+#     :param english_text: input string
+#     :return: french_text string
+#     """
+#     french_text = MyMemoryTranslator(source="en", target="fr").translate(english_text)
+#     print(french_text)
+
+#     return french_text
+
+# # french to english
+# def french_to_english(french_text):
+#     """
+#     function to translate french to english
+#     :param french_text: input string
+#     :return: english_text string
+#     """
+#     english_text = MyMemoryTranslator(source="fr", target="en").translate(french_text)
+#     print(english_text)
+#     return english_text
